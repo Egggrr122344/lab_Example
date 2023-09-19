3c9a7381492125af37039c3d42e627929bcfc60b
root <egor-medvedev00@mail.ru>
2023-09-19 20:16:29 +0000

    tests

--- test/hello_test.cpp
+++ test/hello_test.cpp
@@ -1,14 +1,33 @@
 #include <gtest/gtest.h>
+#include "function.h"
 
-
-TEST(HelloTest, BasicAssertions) {
+TEST(ExampleOfStandartUsageTest1, BasicAssertions) {
   // arrange 
-  int first = 2;
-  int second = 5;
+  unsigned long int number = 10;
+
 
   // act
-  int sum = first + second;
-
+  string result = Get_To_Str(number);
   // assert
-  EXPECT_EQ(sum, 7);
+  EXPECT_EQ(result, "00000000000000000000000000001010");
+}
+TEST(ExampleOfStandartUsageTest2, BasicAssertions) {
+  // arrange 
+  unsigned long int number = 32 ;
+
+
+  // act
+  string result = Get_To_Str(number);
+  // assert
+  EXPECT_EQ(result, "00000000000000000000000000100000");
+}
+TEST(ExampleOfStandartUsageTest3, BasicAssertions) {
+  // arrange 
+  unsigned long int number = 64;
+
+
+  // act
+  string result = Get_To_Str(number);
+  // assert
+  EXPECT_EQ(result, "00000000000000000000000001000000");
 }
