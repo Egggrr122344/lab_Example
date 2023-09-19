98717340e3667f1ae8b056e96f39211cfc2dd908
root <egor-medvedev00@mail.ru>
2023-09-19 20:22:49 +0000

    main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
-#include <iostream>
+#include "function.h"
+const int SIZE = 32;
+int main () {
+    char arr[SIZE];  // array of letters to output
+    unsigned long  int num; // number
+    cout << "Enter your number: \n";
+    cin >> num;
+    string Arr_Str{Get_To_Str(num)}; // return string
+    Follow(arr, Arr_Str, SIZE);
+    Print_Arr(arr, SIZE);
 
-#include "lib.h"
+    return 0;
 
-using namespace std;
 
-int main() {
-    cout << "Bruh: " << endl
-         << A() << endl;
 }
