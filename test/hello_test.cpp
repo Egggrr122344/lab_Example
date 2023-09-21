0a54d31066df792a72daebb0fcce93a191e5c7b3
root <egor-medvedev00@mail.ru>
2023-09-21 16:00:05 +0000

    4 test

--- test/hello_test.cpp
+++ test/hello_test.cpp
@@ -31,3 +31,13 @@ TEST(ExampleOfStandartUsageTest3, BasicAssertions) {
   // assert
   EXPECT_EQ(result, "00000000000000000000000001000000");
 }
+
+
+  TEST(ExampleOfStandartUsageTest4, BasicAssertions) {
+    // arrange
+    unsigned long int number = 2828281212 ;
+    // act
+    string result {Get_To_Str(number)};
+    // result
+    EXPECT_EQ (result, "10101000100101000010010101111100");
+  }
