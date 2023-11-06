d3427be0f35c6cc5f55c0aae5c2afada002ba2d6
root <egor-medvedev00@mail.ru>
2023-11-06 21:21:45 +0000

    g

--- main.cpp
+++ main.cpp
@@ -7,17 +7,13 @@ int main() {
     Money money1 {0,0,5,5,5};
     Money money2 {1,2,6,7,8};
     Money money3;
-    Money money4 = money1.operator+(money2);
 
     money1.printAmount();
 
     money2.printAmount();
 
     money3.printAmount();
-
-    money1.operator+(money2).printAmount();  
-
-    
+ 
     money4.printAmount();
     
 
