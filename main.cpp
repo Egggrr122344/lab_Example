05b54dea78923b7dbb11b64a4b53124a73966e56
root <egor-medvedev00@mail.ru>
2023-12-15 22:50:58 +0000

    lab5

--- main.cpp
+++ main.cpp
@@ -6,27 +6,6 @@
 #include "/com.docker.devenvironments.code/src/lab0/list.cpp"
 
 int main() {
-  std::map<int, int, std::less<int>, mystd::allocator<int, 1000>> m;
 
-  for (size_t i = 0; i < 10; ++i) {
-    m.insert({i + 10, i});
-  }
-
-  for (auto& e : m) {
-    std::cout << '{' << e.first << ", " << e.second << "}, ";
-  }
-
-  std::cout << std::endl;
-  mystd::list<int, mystd::allocator<int, 1000>> lst;
-  mystd::list<int, mystd::allocator<int, 1000>> a;
-  a = lst;
-
-  lst.push_front(123);
-  a.push_front(228);
-
-  std::cout << a.max_size() << std::endl;
-  std::cout << a.front() << std::endl;
-  std::cout << lst.max_size() << std::endl;
-  std::cout << lst.front() << std::endl;
   return 0;
 }
