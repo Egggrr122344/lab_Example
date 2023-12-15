4cd117a49dc2f5f1a55b4becf23fe8ff9c1198f2
root <egor-medvedev00@mail.ru>
2023-12-15 17:28:31 +0000

    lab5

--- main.cpp
+++ main.cpp
@@ -1,30 +1,32 @@
 #include <iostream>
-#include <memory>
-#include "Vector.h"
-#include "Octagon.h"
-#include "Hexagon.h"
-#include <Triangle.h>
-#include "functions.h"
-#include "Point.h"
-#include "/com.docker.devenvironments.code/src/lab0/functions.cpp"
-#include "/com.docker.devenvironments.code/src/lab0/Point.cpp"
-#include "/com.docker.devenvironments.code/src/lab0/Vector.cpp"
-#include "/com.docker.devenvironments.code/src/lab0/Triangle.cpp"
-#include "/com.docker.devenvironments.code/src/lab0/Hexagon.cpp"
-using namespace std;
-using triangle = Triangle<double>;
-using hexagon = Hexagon<double>;
-
-using point = Point<double>;
+#include <map>
+#include <allocator.h>
+#include "/com.docker.devenvironments.code/src/lab0/allocator.cpp"
+#include <list.h>
+#include "/com.docker.devenvironments.code/src/lab0/list.cpp"
 
 int main() {
-  shared_ptr<triangle> t1 = make_shared<triangle>(point(1, 1), point(6, 1 + sqrt(3) * 5), point(11, 1));
-  shared_ptr<triangle> t0 = make_shared<triangle>(point(0, 0), point(5, sqrt(3) * 5), point(10, 0));
-  shared_ptr<hexagon> s0 = make_shared<hexagon>(point(10,10), point(9,10), point(8.5, 10 - std::sqrt(3)/2), point(10.5, 10 - std::sqrt(3)/2), point(9, 10 - std::sqrt(3)), point(10, 10 - std::sqrt(3)));
-  
-  Vector<shared_ptr<Figure<double>>> v = {t1, t0, s0};
-  print_all_areas(v);
-  print_all_centres(v);
-  double all_area = calculate_all_area(v);
-  cout << "Sum of areas: " << all_area << endl;
+  std::map<int, int, std::less<int>, mystd::allocator<int, 1000>> m;
+
+  for (size_t i = 0; i < 10; ++i) {
+    m.insert({i + 10, i});
+  }
+
+  for (auto& e : m) {
+    std::cout << '{' << e.first << ", " << e.second << "}, ";
+  }
+
+  std::cout << std::endl;
+  mystd::list<int, mystd::allocator<int, 1000>> lst;
+  mystd::list<int, mystd::allocator<int, 1000>> a;
+  a = lst;
+
+  lst.push_front(123);
+  a.push_front(228);
+
+  std::cout << a.max_size() << std::endl;
+  std::cout << a.front() << std::endl;
+  std::cout << lst.max_size() << std::endl;
+  std::cout << lst.front() << std::endl;
+  return 0;
 }
